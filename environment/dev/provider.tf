10244de1e1a21bfa4f0025293c7f55c94a5da199
nahid9778 <luckyguy.ali20@gmail.com>
2026-06-11 20:42:30 +0530

    add updated code1

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -8,7 +8,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "backed-infra-rg"
+    resource_group_name  = "backend-infra-rg"
     storage_account_name = "backendstorageaccount2"
     container_name       = "tfstate"
     key                  = "infra.terraform.tfstate"
